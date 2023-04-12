9f2a3aff6d369f402a7a753039beacc7c04098c4
kaanyvz <kaan_yavuz12@hotmail.com>
2023-04-12 19:39:20 +0300

    .

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
@@ -1,11 +1,13 @@
 package guru.springframework.spring6restmvc.controller;
 
 import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.model.BeerDTO;
 import guru.springframework.spring6restmvc.model.CustomerDTO;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
