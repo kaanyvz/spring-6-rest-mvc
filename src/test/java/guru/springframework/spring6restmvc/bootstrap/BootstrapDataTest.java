906431b6e1434b70968d520b7169b69386b81450
kaanyvz <kaan_yavuz12@hotmail.com>
2023-04-20 20:56:29 +0300

    .

--- src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -2,14 +2,13 @@ package guru.springframework.spring6restmvc.bootstrap;
 
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
-import lombok.Data;
+import guru.springframework.spring6restmvc.services.BeerCsvService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class BootstrapDataTest {
@@ -20,17 +19,25 @@ class BootstrapDataTest {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    BeerCsvService csvService;
+
     BootstrapData bootstrapData;
 
     @BeforeEach
-    void setUp(){
-        bootstrapData = new BootstrapData(beerRepository, customerRepository);
+    void setUp() {
+        bootstrapData = new BootstrapData(beerRepository, customerRepository, csvService);
     }
 
     @Test
     void Testrun() throws Exception {
         bootstrapData.run(null);
+
         assertThat(beerRepository.count()).isEqualTo(3);
         assertThat(customerRepository.count()).isEqualTo(3);
     }
 }
+
+
+
+
