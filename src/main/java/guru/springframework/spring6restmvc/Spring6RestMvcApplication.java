9c16faa17ef0b75360c784a20618d3c4ef4335c9
kaanyvz <kaan_yavuz12@hotmail.com>
2023-05-03 18:57:00 +0300

    .

--- src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
+++ src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
@@ -3,10 +3,13 @@ package guru.springframework.spring6restmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class Spring6RestMvcApplication {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		SpringApplication.run(Spring6RestMvcApplication.class, args);
 	}
 
